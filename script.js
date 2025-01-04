2c9541479958bdc4b9278d87fc6e60f5a043c1fb
Ginny-Siix <ginnysiix45@gmail.com>
2025-01-04 18:52:41 +0100

    18h52 test

--- script.js
+++ script.js
@@ -145,4 +145,5 @@ document.onkeydown = function handleKeyDown(e) {
   }
   snakee.setDirection(newDirection); // Met à jour la direction du serpent
 };
-// 04/01/202
+// 04/01/2025
+//test du pc portable
